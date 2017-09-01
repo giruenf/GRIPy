d166cebf1e180409458e959833e2cbe9ee3b6f32
Adriano Paulo <adrianopaulo@gmail.com>
2017-09-01 14:33:47 -0300

    Adding .pyd files

--- Algo/AVO/__init__.py
+++ Algo/AVO/__init__.py
@@ -2,5 +2,5 @@
 
 #from avo_adriano import * 
 
-#from angle import angle_pp, angle_pppsv
-#from avo import avopp, avopp_v2, avops, avopp_ps, avopp_ps_v2
+from angle import angle_pp, angle_pppsv
+from avo import avopp, avopp_v2, avops, avopp_ps, avopp_ps_v2
